2a9c9aa0518e37972763b4044b7fc70435a58dc5
ViltLaravel <nicoleamoguis15@gmail.com>
2024-05-17 14:20:44 +0800

    @init

---
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use Inertia\Inertia;
+use App\Http\Controllers\Controller;
+
+class DashboardController extends Controller
+{
+    public function index() {
+        return Inertia::render('Dashboard');
+    }
+}
